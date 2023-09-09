e04d8eba3f7b05db1c397aa33f0e6e975fbd5563
RobotComp.ru <luchik.sakuras@yandex.ru>
2023-09-09 12:39:12 +0300

    sprint 8

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search: Button = findViewById<Button>(R.id.search)
+        val media_library: Button = findViewById<Button>(R.id.media_library)
+        val settings: Button = findViewById<Button>(R.id.settings)
+
+        search.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(displayIntent)
+            }
+        })
+
+        media_library.setOnClickListener {
+            val displayIntent = Intent(this@MainActivity, MediaLibraryActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        settings.setOnClickListener {
+            val displayIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
     }
 }
+
