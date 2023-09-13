a6ab820882f6c9559ecbc2418c82471db3ad00bb
RobotComp.ru <luchik.sakuras@yandex.ru>
2023-09-13 15:21:59 +0300

    sprint 8.2

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -10,27 +10,27 @@ import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val search: Button = findViewById<Button>(R.id.search)
-        val media_library: Button = findViewById<Button>(R.id.media_library)
-        val settings: Button = findViewById<Button>(R.id.settings)
+        val buttonSearch: Button = findViewById<Button>(R.id.buttonSearch)
+        val buttonMediaLibrary: Button = findViewById<Button>(R.id.buttonMediaLibrary)
+        val buttonSettings: Button = findViewById<Button>(R.id.buttonSettings)
 
-        search.setOnClickListener(object : View.OnClickListener {
+        buttonSearch.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View?) {
                 val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
                 startActivity(displayIntent)
             }
         })
 
-        media_library.setOnClickListener {
+        buttonMediaLibrary.setOnClickListener {
             val displayIntent = Intent(this@MainActivity, MediaLibraryActivity::class.java)
             startActivity(displayIntent)
         }
 
-        settings.setOnClickListener {
+        buttonSettings.setOnClickListener {
             val displayIntent = Intent(this@MainActivity, SettingsActivity::class.java)
             startActivity(displayIntent)
         }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,17 +1,20 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val back: Button = findViewById<Button>(R.id.back)
-        back.setOnClickListener {
+        val buttonBack: LinearLayout = findViewById<LinearLayout>(R.id.buttonBack)
+        buttonBack.setOnClickListener {
             finish()
         }
     }
